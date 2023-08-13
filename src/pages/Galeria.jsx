5416e1f59c4dd5460c2b7720b15a104f10cf46f3
IgnaDev <arcortoon@gmail.com>
2023-08-12 22:11:31 -0300

    useFetch aplicado y funcionando

--- src/pages/Galeria.jsx
+++ src/pages/Galeria.jsx
@@ -1,24 +1,29 @@
 import GaleriaImg from "../components/GaleriaImg"
 import useFetch from "../hooks/useFetch"
-
+import "../styles/Galeria.css"
 
 export default function Galeria() {
     const { imagenes, getImagenes } = useFetch()
 
+    document.addEventListener("DOMContentLoaded", () => {
+        getImagenes(10)
+    })
     return (
-        <>
+        <div className="gal-home">
             <h1>Galeria</h1>
-            <button onClick={
-                () => {
-                     getImagenes(10)
-                }
-            }>Obtener imagenes</button>
+
             <div className="galeria">
                 {
                     imagenes ? 
                     imagenes.map(imagen => <GaleriaImg key={imagen.id} imagen={imagen}/>) : <p>No hay imagenes</p>
                 }
             </div>
-        </>
+
+            <button className="boton-de-mas" onClick={
+                () => {
+                     getImagenes(10)
+                }
+            }>+</button>
+        </div>
     )
 }
